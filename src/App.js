7c25dbb823dc264e7a5ef1e98594d8016799b08f
Takashi Ikki <new_ittyandesu@outlook.jp>
2021-03-31 00:26:39 +0900

    link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Catalog from './Catalog';
+import About from './About';
+import {Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">The Catalog App</h1>
+        <nav>
+            <ul>
+              <li><Link to='/'>Catalog</Link></li>
+              <li><Link to='/about'>About</Link></li>
+            </ul>
+          </nav>
       </header>
+      <Switch>
+        <Route exact path='/' component={Catalog} />
+        <Route exact path='/about' component={About} />
+      </Switch>
     </div>
   );
 }
